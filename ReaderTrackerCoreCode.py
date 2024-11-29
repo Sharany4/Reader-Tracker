9b6e8e24bb7194395b31ed6697533b2454a9c822
Sharanya <sharanyapanda@gmail.com>
2024-11-29 17:08:56 +1300

    Creating Book class

--- ReaderTrackerCoreCode.py
+++ ReaderTrackerCoreCode.py
@@ -10,6 +10,17 @@
 # This class will have a title, author, year
 # It will also store what collections it is apart of
 # It will also havea to string method to represent itself
+class Book:
+    def __init__(self, title: str, author: str, year: int):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def printBook(self):
+        print(f"Book Details: Title: {self.title}, Author: {self.author}, Year: {self.year}")
+
+
+
 
 
 # TODO Create a BookCollection class
