08800b4155e645b01f4f842854f2850862f5087d
Sharanya <sharanyapanda@gmail.com>
2024-11-29 16:10:43 +1300

    Added comments for Book class and Book Collection class to be created

---
+++ ReaderTrackerCoreCode.py
@@ -0,0 +1,22 @@
+# In this file I am creating the core functionality for my project
+
+
+# My initial implementation will be to make the user
+# able to add and remove from a tobe read list ,and move to a read list.
+# Later this will be expanded so the can create custom lists.
+
+
+# TODO Create a Book class
+# This class will have a title, author, year
+# It will also store what collections it is apart of
+# It will also havea to string method to represent itself
+
+
+# TODO Create a BookCollection class
+# hTHis class will represent a collection of books.
+# It will provide functionality to add and remove books, and sort them
+# how the user customizes.
+# Will need to figure out how to book books from one collection to another ones read
+
+
+
