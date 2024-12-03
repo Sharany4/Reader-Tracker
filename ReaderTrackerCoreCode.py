78f8741987e736795188338c84a4754573888465
Sharanya <sharanyapanda@gmail.com>
2024-12-03 14:43:06 +1300

    wrote comments for library class to create

--- ReaderTrackerCoreCode.py
+++ ReaderTrackerCoreCode.py
@@ -21,16 +21,17 @@ class Book:
     # TODO add function that notes a book is read(removes from collections and adds to the read list)
     # To be finished once JSON saving is implemented
     def note_book_as_read(self):
-        #add book to json of completed books
+        # add book to json of completed books
         for coll in self.collections:
             coll.remove_book(self)
 
     def add_collection(self, coll: "BookCollection"):
         self.collections.append(coll)
 
-
     def get_book_details(self):
         return f"Book Details: Title: {self.title}, Author: {self.author}, Year: {self.year}"
+
+    # TODO: Create method to serialise and deserialise a book object
 
 
 # This class will represent a collection of books.
@@ -40,6 +41,7 @@ class Book:
 class BookCollection:
     def __init__(self):
         self.books = []  # Initialize the empty list
+        # add it to the collections master json file
 
     def add_book(self, book: Book):
         if not isinstance(book, Book):
@@ -47,6 +49,7 @@ class BookCollection:
         if book in self.books:
             raise DuplicateError(f" Book {book} is already in the collection")
         self.books.append(book)
+        # add to collections json file
         self.books.sort(key=lambda x: (x.author, x.title))  # Sorts the books everytime another book is added
 
     def remove_book(self, book: Book):
@@ -55,10 +58,27 @@ class BookCollection:
         if book not in self.books:
             raise ValueError("The book is not in the collection")
         self.books.remove(book)
+        # remove to collections json file
 
     def print_collection(self):
         for b in self.books:
             print(b.get_book_details())
+
+    # TODO: Create method to serialise and deserialise a book object
+
+
+# TODO: create library class
+# Library class works as the headmaster to manage collections and books
+# class Library:
+# def __int__(self):
+# load up GUI
+# let user see collctions to view
+# let user click collections and books to makr as read
+# load collections
+# add collection
+# delete collection
+# add book to this collection
+# remove book from this collection
 
 
 # class for creating a exception when trying to add a duplicate
